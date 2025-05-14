a92186f6594c51a2ccabbaad424adabbc32786be
nanthakumar-dev <nantha656565@gmail.com>
2025-05-14 01:59:46 -0700

    okll

--- backend/app.js
+++ backend/app.js
@@ -25,9 +25,9 @@ app.use('/api/v1',orderRoute)
 app.use('/api/v1',paymentRoute)
 
 
-    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+    app.use(express.static(path.join(__dirname,'./dist')))
     app.get('/',(req,res)=>{
-        res.sendFile(path.join(__dirname,'../frontend/dist/index.html'))
+        res.sendFile(path.join(__dirname,'./dist/index.html'))
     }
     )
 
